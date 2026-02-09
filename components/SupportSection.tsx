4f1e63f75ba08e396770443f2e62cddc3badfa03
Anushaburnwal <anushaburnwal84@gmail.com>
2026-02-09 10:49:19 +0530

    buttons got fixed

--- components/SupportSection.tsx
+++ components/SupportSection.tsx
@@ -11,26 +11,30 @@ export default function SupportSection() {
 
       {/* RIGHT CONTENT */}
       <div className={styles.content}>
-        <h2>You don't have to do this all alone.</h2>
+        <div className="px-14">
 
-        <p className={styles.subtitle}>
-          If you are facing any of these, there's hope:
-        </p>
+          <h2>You don't have to do this all alone.</h2>
 
-        <ul>
-          <li>Persistent feelings of sadness or hopelessness</li>
-          <li>Trouble focusing or making decisions</li>
-          <li>Difficulty maintaining relationships</li>
-          <li>Feeling constantly exhausted or unmotivated</li>
-          <li>A pervasive sense of being overwhelmed</li>
-        </ul>
+          <p className={styles.subtitle}>
+            If you are facing any of these, there's hope:
+          </p>
 
-        <p className={styles.footerText}>
-          With empathy and guidance, we’ll work together to navigate the
-          challenges life throws your way.
-        </p>
+          <ul>
+            <li>Persistent feelings of sadness or hopelessness</li>
+            <li>Trouble focusing or making decisions</li>
+            <li>Difficulty maintaining relationships</li>
+            <li>Feeling constantly exhausted or unmotivated</li>
+            <li>A pervasive sense of being overwhelmed</li>
+          </ul>
 
-        <button className={styles.cta}>
+          <p className={styles.footerText}>
+            With empathy and guidance, we’ll work together to navigate the
+            challenges life throws your way.
+          </p>
+
+        </div>
+
+        <button className={`${styles.cta} mt-auto`}>
           WORK WITH ME →
         </button>
       </div>
